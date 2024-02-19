5d8e179541cc4eb015f32ab3f2c81c5a6ee8c4e8
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-02-19 09:13:00 +0900

    udemy練習-section1-17 円を動かす2

---
+++ src/pages/code/udemy-practice/section1/17/index.tsx
@@ -0,0 +1,24 @@
+import { NextReactP5Wrapper } from '@p5-wrapper/next'
+import { type Sketch } from '@p5-wrapper/react'
+
+const sketch: Sketch = (p5) => {
+  p5.preload = () => {}
+
+  p5.setup = () => {
+    p5.frameRate(1) // 1 frame per second
+    p5.createCanvas(400, 300)
+  }
+
+  let locationX = 0 // x座標
+  let velocityX = 30 // 速度
+  p5.draw = () => {
+    p5.background(0)
+    p5.ellipse(locationX, 50, 80, 80)
+    locationX += velocityX
+  }
+}
+
+const index = () => {
+  return <NextReactP5Wrapper sketch={sketch} />
+}
+export default index

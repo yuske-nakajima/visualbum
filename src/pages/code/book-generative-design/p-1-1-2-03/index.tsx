098de0778fcd5932a5edc76f6287b4472f3c383b
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-03-03 02:37:27 +0900

    書籍-Generative Design with p5.js: P.1.1.2 円形に配置した色のスペクトルの応用 三角関数バリバリ sin・cosの値を表示

--- src/pages/code/book-generative-design/p-1-1-2-03/index.tsx
+++ src/pages/code/book-generative-design/p-1-1-2-03/index.tsx
@@ -1,7 +1,6 @@
 import { NextReactP5Wrapper } from '@p5-wrapper/next'
 import { type Sketch } from '@p5-wrapper/react'
-
-const ballSize = 10
+import { Vector } from 'p5'
 
 const sketch: Sketch = (p5) => {
   p5.setup = () => {
@@ -9,27 +8,28 @@ const sketch: Sketch = (p5) => {
   }
 
   p5.draw = () => {
-    p5.background('#fff')
+    p5.background('#F2F4F8')
     p5.colorMode(p5.HSB, 360, p5.width, p5.height)
 
-    const radius = p5.width / 3
-    const diameter = p5.width / 2
-    p5.ellipse(diameter, diameter, radius * 2)
+    const radius = (p5.width < p5.height ? p5.width : p5.height) / 3
+    const diameter: Vector = p5.createVector(p5.width / 2, p5.height / 2)
+    p5.ellipse(diameter.x, diameter.y, radius * 2)
     // 円の補助線
-    p5.line(diameter - radius, diameter, diameter + radius, diameter)
-    p5.line(diameter, diameter - radius, diameter, diameter + radius)
+    p5.line(diameter.x - radius, diameter.y, diameter.x + radius, diameter.y)
+    p5.line(diameter.x, diameter.y - radius, diameter.x, diameter.y + radius)
 
-    const value = p5.frameCount % 360
-    const sin = p5.sin(p5.radians(value))
-    const cos = p5.cos(p5.radians(value))
-    const vx = diameter + cos * radius
-    const vy = diameter + sin * radius
+    const degree = p5.frameCount % 360
+    const radians = p5.radians(degree)
+    const sin = p5.sin(radians)
+    const cos = p5.cos(radians)
+    const vx = diameter.x + cos * radius
+    const vy = diameter.y + -sin * radius
 
     // 半径
     p5.push()
     p5.strokeWeight(6)
     p5.stroke(360, p5.width, p5.height)
-    p5.line(diameter, diameter, vx, vy)
+    p5.line(diameter.x, diameter.y, vx, vy)
     p5.pop()
 
     // sin
@@ -38,12 +38,12 @@ const sketch: Sketch = (p5) => {
 
     p5.push()
     p5.strokeWeight(6)
-    // p5.line(diameter, diameter, diameter, vy)
+    p5.line(diameter.x, diameter.y, diameter.x, vy)
     p5.pop()
     // sin-補助線
     p5.push()
     p5.drawingContext.setLineDash([10, 2])
-    p5.line(vx, vy, diameter, vy)
+    p5.line(vx, vy, diameter.x, vy)
     p5.pop()
 
     p5.pop()
@@ -54,18 +54,36 @@ const sketch: Sketch = (p5) => {
 
     p5.push()
     p5.strokeWeight(6)
-    // p5.line(diameter, diameter, vx, diameter)
+    p5.line(diameter.x, diameter.y, vx, diameter.y)
     p5.pop()
     // cos-補助線
     p5.push()
     p5.drawingContext.setLineDash([10, 2])
-    p5.line(vx, vy, vx, diameter)
+    p5.line(vx, vy, vx, diameter.y)
     p5.pop()
 
     p5.pop()
 
-    p5.text(`sin: ${sin.toFixed(2)}`, 10, 20)
-    p5.text(`cos: ${cos.toFixed(2)}`, 10, 40)
+    p5.push()
+    p5.textSize(20)
+
+    p5.push()
+    p5.stroke(120, p5.width, p5.height)
+    p5.text(`sin: ${sin.toFixed(2)}`, 10, 30)
+    p5.pop()
+
+    p5.push()
+    p5.stroke(240, p5.width, p5.height)
+    p5.text(`cos: ${cos.toFixed(2)}`, 10, 60)
+    p5.pop()
+
+    p5.push()
+    p5.stroke('#555')
+    p5.text(`degree: ${degree.toFixed(2)}`, 10, 90)
+    p5.text(`radians: ${radians.toFixed(2)}`, 10, 120)
+    p5.pop()
+
+    p5.pop()
   }
 }
 

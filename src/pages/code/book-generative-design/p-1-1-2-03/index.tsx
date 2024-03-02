ebaba6a271bd7d9dbf65226c8484bd120de66fa2
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-03-02 14:31:30 +0900

    書籍-Generative Design with p5.js: P.1.1.2 円形に配置した色のスペクトルの応用 三角関数バリバリ

--- src/pages/code/book-generative-design/p-1-1-2-03/index.tsx
+++ src/pages/code/book-generative-design/p-1-1-2-03/index.tsx
@@ -1,9 +1,12 @@
 import { NextReactP5Wrapper } from '@p5-wrapper/next'
 import { type Sketch } from '@p5-wrapper/react'
 
+const ballSize = 10
+
 const sketch: Sketch = (p5) => {
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth - 22, p5.windowHeight - 22)
+    p5.frameRate(30)
   }
 
   p5.draw = () => {
@@ -12,29 +15,52 @@ const sketch: Sketch = (p5) => {
 
     const radius = p5.width / 3
     const diameter = p5.width / 2
-    p5.ellipse(
-      diameter + p5.sin(p5.radians(180)) * radius,
-      diameter + p5.cos(p5.radians(90)) * radius,
-      radius * 2,
-    )
+    p5.ellipse(diameter, diameter, radius * 2)
+    // 円の補助線
+    p5.line(diameter - radius, diameter, diameter + radius, diameter)
+    p5.line(diameter, diameter - radius, diameter, diameter + radius)
 
     const value = p5.frameCount % 360
     const vx = diameter + p5.sin(p5.radians(value)) * radius
     const vy = diameter + p5.cos(p5.radians(value)) * radius
 
-    // p5.line(diameter, diameter, vx, vy)
-    p5.line(vx, vy, vx, diameter)
-    p5.line(vx, vy, diameter, vy)
-    p5.line(diameter, diameter, vx, diameter)
-    p5.line(diameter, diameter, diameter, vy)
+    // 半径
+    p5.push()
+    p5.strokeWeight(6)
+    p5.stroke(360, p5.width, p5.height)
+    p5.line(diameter, diameter, vx, vy)
+    p5.pop()
+
+    // sin
+    p5.push()
+    p5.stroke(120, p5.width, p5.height)
 
     p5.push()
-    p5.fill(value, p5.mouseX, p5.mouseY)
-    p5.noStroke()
-    p5.ellipse(diameter, diameter, 10)
-    p5.ellipse(vx, vy, 30)
-    p5.ellipse(vx, diameter, 10)
-    p5.ellipse(diameter, vy, 10)
+    p5.strokeWeight(6)
+    p5.line(diameter, diameter, diameter, vy)
+    p5.pop()
+    // sin-補助線
+    p5.push()
+    p5.drawingContext.setLineDash([10, 2])
+    p5.line(vx, vy, diameter, vy)
+    p5.pop()
+
+    p5.pop()
+
+    // cos
+    p5.push()
+    p5.stroke(240, p5.width, p5.height)
+
+    p5.push()
+    p5.strokeWeight(6)
+    p5.line(diameter, diameter, vx, diameter)
+    p5.pop()
+    // cos-補助線
+    p5.push()
+    p5.drawingContext.setLineDash([10, 2])
+    p5.line(vx, vy, vx, diameter)
+    p5.pop()
+
     p5.pop()
   }
 }

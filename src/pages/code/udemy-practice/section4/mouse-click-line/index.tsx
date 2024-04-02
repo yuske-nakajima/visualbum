d3cb9a760c8f3080ddc5ee8f84812b0d015125e1
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-04-03 08:40:01 +0900

    udemy練習-section4 マウス操作1

---
+++ src/pages/code/udemy-practice/section4/mouse-click-line/index.tsx
@@ -0,0 +1,41 @@
+import { NextReactP5Wrapper } from '@p5-wrapper/next'
+import { type Sketch } from '@p5-wrapper/react'
+
+const sketch: Sketch = (p5) => {
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth - 22, p5.windowHeight - 22)
+  }
+
+  let mousePressed = false
+  let mousePressedX = 0
+  let mousePressedY = 0
+
+  p5.draw = () => {
+    // console.log('mouseX:', p5.mouseX, 'mouseY:', p5.mouseY)
+    p5.stroke(0)
+
+    if (p5.mouseIsPressed) {
+      if (!mousePressed) {
+        mousePressedX = p5.mouseX
+        mousePressedY = p5.mouseY
+        console.log('mouseIsPressed', p5.mouseX, p5.mouseY)
+      }
+      mousePressed = true
+    } else {
+      mousePressed = false
+    }
+  }
+
+  p5.mouseClicked = (e: MouseEvent) => {
+    p5.beginShape()
+    p5.vertex(mousePressedX, mousePressedY)
+    p5.vertex(e.x, e.y)
+    p5.endShape()
+    return false
+  }
+}
+
+const index = () => {
+  return <NextReactP5Wrapper sketch={sketch} />
+}
+export default index

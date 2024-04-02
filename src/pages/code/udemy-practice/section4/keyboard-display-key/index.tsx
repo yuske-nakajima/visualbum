23aedfa4c73c6af96b9e0869271ef28a4304facd
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-04-03 08:57:03 +0900

    udemy練習-section4 キー操作-押したキーを表示

---
+++ src/pages/code/udemy-practice/section4/keyboard-display-key/index.tsx
@@ -0,0 +1,27 @@
+import { NextReactP5Wrapper } from '@p5-wrapper/next'
+import { type Sketch } from '@p5-wrapper/react'
+import { Vector } from 'p5'
+
+const sketch: Sketch = (p5) => {
+  let textVector: Vector
+
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth - 22, p5.windowHeight - 22)
+    p5.colorMode(p5.HSB)
+    p5.fill(0, 50, 90)
+    p5.textSize(p5.width / 4)
+    p5.rectMode(p5.CENTER)
+
+    textVector = p5.createVector(20, p5.height / 2)
+  }
+
+  p5.draw = () => {
+    p5.background(180, 5, 96)
+    p5.text(p5.key, textVector.x, textVector.y)
+  }
+}
+
+const index = () => {
+  return <NextReactP5Wrapper sketch={sketch} />
+}
+export default index

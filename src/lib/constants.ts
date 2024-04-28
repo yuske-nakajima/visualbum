e5e630644005a5ab8a9d241ba79fe40293efc9a0
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-04-28 13:01:34 +0900

    インデックス表示ロジックを修正

---
+++ src/lib/constants.ts
@@ -0,0 +1,145 @@
+import { PageInfo } from '@/lib/types'
+
+export const PAGE_INFO_LIST: PageInfo[] = [
+  { title: 'ant', href: '/code/ant' },
+  { title: 'box', href: '/code/box' },
+  { title: 'circle-ball', href: '/code/circle-ball' },
+  { title: 'circle-line', href: '/code/circle-line' },
+  { title: 'fall-kanji', href: '/code/fall-kanji' },
+  { title: 'grid-text', href: '/code/grid-text' },
+  { title: 'hello', href: '/code/hello' },
+  { title: 'like-reversi', href: '/code/like-reversi' },
+  { title: 'random-ball', href: '/code/random-ball' },
+  { title: 'swirl', href: '/code/swirl' },
+  { title: 'trigonometry', href: '/code/trigonometry' },
+  { title: 'wave/sine-curve', href: '/code/wave/sine-curve' },
+  { title: 'wave/sine-curve2', href: '/code/wave/sine-curve2' },
+  {
+    title: 'book-generative-design/p-1-0-01',
+    href: '/code/book-generative-design/p-1-0-01',
+  },
+  {
+    title: 'book-generative-design/p-1-1-1-01',
+    href: '/code/book-generative-design/p-1-1-1-01',
+  },
+  {
+    title: 'book-generative-design/p-1-1-2-01',
+    href: '/code/book-generative-design/p-1-1-2-01',
+  },
+  {
+    title: 'book-generative-design/p-1-1-2-02',
+    href: '/code/book-generative-design/p-1-1-2-02',
+  },
+  {
+    title: 'book-generative-design/p-1-1-2-03',
+    href: '/code/book-generative-design/p-1-1-2-03',
+  },
+  {
+    title: 'book-generative-design/p-1-2-1-01',
+    href: '/code/book-generative-design/p-1-2-1-01',
+  },
+  {
+    title: 'book-generative-design/p-1-2-3-01',
+    href: '/code/book-generative-design/p-1-2-3-01',
+  },
+  {
+    title: 'udemy-practice/section1/15',
+    href: '/code/udemy-practice/section1/15',
+  },
+  {
+    title: 'udemy-practice/section1/16',
+    href: '/code/udemy-practice/section1/16',
+  },
+  {
+    title: 'udemy-practice/section1/17',
+    href: '/code/udemy-practice/section1/17',
+  },
+  {
+    title: 'udemy-practice/section1/18',
+    href: '/code/udemy-practice/section1/18',
+  },
+  {
+    title: 'udemy-practice/section1/19',
+    href: '/code/udemy-practice/section1/19',
+  },
+  {
+    title: 'udemy-practice/section1/21',
+    href: '/code/udemy-practice/section1/21',
+  },
+  {
+    title: 'udemy-practice/section2/22',
+    href: '/code/udemy-practice/section2/22',
+  },
+  {
+    title: 'udemy-practice/section2/23',
+    href: '/code/udemy-practice/section2/23',
+  },
+  {
+    title: 'udemy-practice/section2/24',
+    href: '/code/udemy-practice/section2/24',
+  },
+  {
+    title: 'udemy-practice/section2/25',
+    href: '/code/udemy-practice/section2/25',
+  },
+  {
+    title: 'udemy-practice/section2/26',
+    href: '/code/udemy-practice/section2/26',
+  },
+  {
+    title: 'udemy-practice/section2/28',
+    href: '/code/udemy-practice/section2/28',
+  },
+  {
+    title: 'udemy-practice/section2/29',
+    href: '/code/udemy-practice/section2/29',
+  },
+  {
+    title: 'udemy-practice/section2/33',
+    href: '/code/udemy-practice/section2/33',
+  },
+  {
+    title: 'udemy-practice/section3/35-38',
+    href: '/code/udemy-practice/section3/35-38',
+  },
+  {
+    title: 'udemy-practice/section3/39',
+    href: '/code/udemy-practice/section3/39',
+  },
+  {
+    title: 'udemy-practice/section3/mouse-clicked.tsx',
+    href: '/code/udemy-practice/section3/mouse-clicked.tsx',
+  },
+  {
+    title: 'udemy-practice/section3/mouse-dragged.tsx',
+    href: '/code/udemy-practice/section3/mouse-dragged.tsx',
+  },
+  {
+    title: 'udemy-practice/section4/keyboard-background-change',
+    href: '/code/udemy-practice/section4/keyboard-background-change',
+  },
+  {
+    title: 'udemy-practice/section4/keyboard-display-key',
+    href: '/code/udemy-practice/section4/keyboard-display-key',
+  },
+  {
+    title: 'udemy-practice/section4/mouse-click-line',
+    href: '/code/udemy-practice/section4/mouse-click-line',
+  },
+  {
+    title: 'udemy-practice/section5/attractor1',
+    href: '/code/udemy-practice/section5/attractor1',
+  },
+  {
+    title: 'udemy-practice/section5/attractor2',
+    href: '/code/udemy-practice/section5/attractor2',
+  },
+  {
+    title: 'udemy-practice/section5/attractor3',
+    href: '/code/udemy-practice/section5/attractor3',
+  },
+  {
+    title: 'udemy-practice/section5/attractor4',
+    href: '/code/udemy-practice/section5/attractor4',
+  },
+]

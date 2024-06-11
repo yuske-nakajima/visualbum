7c2f63a2d65866b9934c5a38562bbb2042dddcb9
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-06-11 12:38:02 +0900

    vercel analytics を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Menu from '@/lib/component/menu'
+import { Analytics } from '@vercel/analytics/react'
 import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Head>
         <title>Visualbum</title>
         <meta name='description' content='p5.jsの習作' />
